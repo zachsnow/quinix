1d0e97e01ef4f1c504f32666a4d2e97268a970e5
Zach Snow <z@zachsnow.com>
2020-04-27 14:53:11 -0700

    Fix kernel build script when not passing --compiled.

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 
 QLLC_PATH="../bin/qllc.ts"
 QASM_PATH="../bin/qasm.ts"
-if [ "$1" == "--compiled" ]; then
+if [ "${1-}" == "--compiled" ]; then
   QLLC_PATH="../build/bin/qllc.js"
   QASM_PATH="../build/bin/qasm.js"
 fi
