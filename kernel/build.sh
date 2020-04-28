c9f7775ab842f6d4dee4ba6db499a1b756d4ccc9
Zach Snow <z@zachsnow.com>
2020-04-27 22:40:55 -0700

    Kernel hacking.

--- kernel/build.sh
+++ kernel/build.sh
@@ -14,7 +14,7 @@ echo "Assembling test kernel..."
 ${QASM_PATH} -o test-kernel.qbin out.qasm support.qasm
 
 echo "Compiling kernel..."
-${QLLC_PATH} kernel.qll support.qll scheduler.qll
+${QLLC_PATH} kernel.qll support.qll scheduler.qll peripherals.qll
 
 echo "Assembling kernel..."
 ${QASM_PATH} -o kernel.qbin out.qasm support.qasm
