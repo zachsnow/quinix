a70937f437a5a26337d57ac2724a94d1ad5b0384
Zach Snow <z@zachsnow.com>
2020-04-27 22:40:07 -0700

    Now that compiled libs aren't in the repo, we need to create the output directory before building.

--- lib/build.sh
+++ lib/build.sh
@@ -1,6 +1,9 @@
 # build.sh: builds the system runtime and standard library.
 set -euo pipefail
 
+# Make sure we have an output directory.
+mkdir -p bin
+
 QLLC_PATH="../bin/qllc.ts"
 if [ "${1-}" == "--compiled" ]; then
   QLLC_PATH="../build/bin/qllc.js"
